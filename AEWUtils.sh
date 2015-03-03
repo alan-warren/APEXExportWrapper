ddc941328c42da1502830685403e3cfcd89ddae6
Alan Warren <awarren@uoguelph.ca>
2015-03-03 10:25:02 -0500

    Glorified stash

--- AEWUtils.sh
+++ AEWUtils.sh
@@ -50,3 +50,18 @@ function pickApp() {
 			return 1
 	fi
 }
+
+function fieldFromCSV() {
+	$headers=$1
+	$line=$2
+	$fieldName=$3
+	echo $headers |awk ' 
+	BEGIN { count=0; quote=0}
+    { for (i=1;i<=length($0);i++)
+      {
+      	  if(substr($0,i,1)=="\"") { quote=!quote; continue};
+          if(substr($0,i,1)=="," && quote==0) {count++;}
+      }
+    }
+    END {print count}'
+}
